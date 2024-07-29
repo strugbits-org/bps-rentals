f45852b549ec58eca9a05ee05dc912dea64bd13b
Osyed002 <osyed@strugbitsglobal.com>
2024-07-29 13:50:55 +0500

    static category and market page

--- src/app/(root)/category/[id]/page.jsx
+++ src/app/(root)/category/[id]/page.jsx
@@ -5,6 +5,19 @@ import { fetchAllCategoriesData, fetchFilteredProducts, getSelectedColorsData } 
 import { getMarketsData } from "@/Services/SectionsApis";
 import { findCategoryData } from "@/Utils/Utils";
 
+export const generateStaticParams = async () => {
+  try {
+    const getSlug = (url) => {
+      return "/" + url.match(/\/category\/(.+)/)[1];
+    }
+    const categoriesData = await fetchAllCategoriesData();
+    const paths = categoriesData.map((data) => ({ slug: getSlug(data.parentCollection["link-copy-of-category-name-2"]) }));
+    return paths;
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
 export default async function Page({ params }) {
   const slug = "/category/" + params.id;
   const categoriesData = await fetchAllCategoriesData();
