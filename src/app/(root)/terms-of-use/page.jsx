c02c081bd88023f84b5622644486bf3d0c14aa5d
attaStrugbits <161705067+attaStrugbits@users.noreply.github.com>
2024-08-07 20:26:47 +0500

    remove terms and privacy page and added not found page

--- src/app/(root)/terms-of-use/page.jsx
+++
@@ -1,7 +0,0 @@
-import TermsAndCondition from "@/components/TermsAndCondition";
-import { getTermsOfUsePageContent } from "@/Services/Index";
-
-export default async function Page() {
-  const [termsOfUse] = await Promise.all([getTermsOfUsePageContent()]);
-  return <TermsAndCondition data={termsOfUse} />;
-}
--- src/components/Error404Page.jsx
+++ src/components/Error404Page.jsx
@@ -1,22 +1,26 @@
 "use client";
 
+import { markPageLoaded } from "@/Utils/AnimationFunctions";
+import { useEffect } from "react";
+
 const Error404Page = () => {
+  useEffect(() => {
+    markPageLoaded();
+  }, []);
   return (
-    <>
-      <section className="section-error-404">
-        <div className="container-title">
-          <h1 className="fs--900 blue-1 split-chars" data-aos="d:loop">
-            <span>404</span>
-          </h1>
-          <span
-            className="fs--20 fw-600 text-uppercase"
-            data-aos="fadeIn .8s ease-in-out .6s"
-          >
-            Page not found
-          </span>
-        </div>
-      </section>
-    </>
+    <section className="section-error-404">
+      <div className="container-title">
+        <h1 className="fs--900 blue-1 split-chars" data-aos="d:loop">
+          <span>404</span>
+        </h1>
+        <span
+          className="fs--20 fw-600 text-uppercase"
+          data-aos="fadeIn .8s ease-in-out .6s"
+        >
+          Page not found
+        </span>
+      </div>
+    </section>
   );
 };
 
