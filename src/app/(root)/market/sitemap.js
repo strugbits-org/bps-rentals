180aa7c4d4eadc7ff494849c0156596a9fb208e8
attaStrugbits <161705067+attaStrugbits@users.noreply.github.com>
2024-08-21 20:10:21 +0500

    added sitemaps

---
+++ src/app/(root)/market/sitemap.js
@@ -0,0 +1,15 @@
+import { getMarketsData } from "@/Services/SectionsApis";
+
+const BASE_URL = process.env.BASE_URL;
+
+export default async function sitemap() {
+  const marketsData = await getMarketsData();
+  const paths = marketsData.map((data) => ({ slug: data.slug }));
+
+  const markets = paths.map(({ slug }) => ({
+    url: `${BASE_URL}/market/${slug}`,
+    lastModified: new Date(),
+  }));
+
+  return [...markets];
+}
