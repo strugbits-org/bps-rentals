a7bcab26c67a3a174951f96bc27e08a212336437
Osyed002 <osyed@strugbitsglobal.com>
2024-09-09 16:08:35 +0500

    quick fix

--- src/app/(root)/product/[slug]/page.jsx
+++ src/app/(root)/product/[slug]/page.jsx
@@ -44,7 +44,7 @@ export async function generateMetadata({ params }) {
 export const generateStaticParams = async () => {
   try {
     const paths = await fetchAllProductsPaths() || [];
-    return paths.slice(0, 1);
+    return paths;
   } catch (error) {
     console.error("Error:", error);
   }
