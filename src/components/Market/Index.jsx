b1026b23a43e8b91b591c96421047ad6dfd3d8f2
Osyed002 <osyed@strugbitsglobal.com>
2024-09-09 21:04:31 +0500

    highlights section issue

--- src/components/Market/Index.jsx
+++ src/components/Market/Index.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { markPageLoaded } from "@/Utils/AnimationFunctions";
 import Markets from "../Common/Sections/MarketSection";
 import Studios from "../Common/Sections/StudiosSection";
@@ -10,6 +10,8 @@ import PeopleReviewSlider from "../Common/Sections/PeopleReviewSlider";
 import { MarketIntroSection } from "./MarketIntroSection";
 import { MarketBestSeller } from "./MarketBestSeller";
 import { MarketSlider } from "./MarketSlider";
+import { getSavedProductData } from "@/Services/ProductsApis";
+import CartModal from "../Common/Modals/CartModal";
 
 const MarketPage = ({
   marketSection,
@@ -21,18 +23,124 @@ const MarketPage = ({
   marketsData,
   peopleReviewSliderData,
   highlightsSectionData,
-  bestSellerProducts
+  bestSellerProducts,
+  bestSellers
 }) => {
-  
+
+  const [savedProductsData, setSavedProductsData] = useState([]);
+  const [selectedProductData, setSelectedProductData] = useState(null);
+  const [productSnapshots, setProductSnapshots] = useState();
+  const [selectedVariantData, setSelectedVariantData] = useState(null);
+  const [selectedVariantIndex, setSelectedVariantIndex] = useState(0);
+  const [productFilteredVariantData, setProductFilteredVariantData] =
+    useState();
+
+  const getSelectedProductSnapShots = async (productData, activeVariant) => {
+    setSelectedProductData(productData);
+    try {
+      const { productSnapshotData, productVariantsData } = productData;
+
+      let dataMap = new Map(
+        productVariantsData.map((item) => [item.sku.toLowerCase(), item])
+      );
+      let filteredVariantData;
+      if (productVariantsData && productData) {
+        filteredVariantData = productData.variantData.filter((variant) => {
+          const normalizedSku = variant.sku.toLowerCase();
+          if (dataMap.has(normalizedSku)) {
+            const dataItem = dataMap.get(normalizedSku);
+            variant.variant.variantId = dataItem._id;
+            return true;
+          }
+          return false;
+        });
+      }
+      setProductSnapshots(productSnapshotData);
+      setProductFilteredVariantData(filteredVariantData);
+      const currentActiveIndex = productData.variantData.findIndex(x => x.variant._id === activeVariant.variant._id);
+      const currentActive = productData.variantData[currentActiveIndex];
+
+      if (filteredVariantData && currentActive && filteredVariantData.length > 0) {
+        handleImageChange({
+          index: currentActiveIndex,
+          selectedVariantData: currentActive.variant,
+          productSnapshots: productSnapshotData,
+          modalUrl: currentActive.zipUrl,
+        });
+      }
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
+  const handleImageChange = ({
+    index,
+    selectedVariantData,
+    productSnapshots,
+    modalUrl,
+  }) => {
+    if (productSnapshots) {
+      const selectedVariantFilteredData = productSnapshots.find(
+        (variant) => variant.colorVariation === selectedVariantData.variantId
+      );
+
+      if (selectedVariantFilteredData && selectedVariantFilteredData?.images) {
+        const combinedVariantData = {
+          ...selectedVariantData,
+          ...selectedVariantFilteredData,
+          modalUrl: modalUrl,
+        };
+
+        setSelectedVariantIndex(index);
+        setSelectedVariantData(combinedVariantData);
+      } else {
+        const combinedVariantData = {
+          ...selectedVariantData,
+          ...selectedVariantFilteredData,
+          modalUrl: modalUrl,
+          images: [{ src: selectedVariantData.imageSrc }],
+        };
+        setSelectedVariantIndex(index);
+        setSelectedVariantData(combinedVariantData);
+      }
+    }
+  };
+
+  const fetchSavedProducts = async () => {
+    try {
+      const savedProducts = await getSavedProductData();
+      setSavedProductsData(savedProducts);
+    } catch (error) {
+      console.log("Error while fetching Saved Product", error);
+    }
+  }
+
   useEffect(() => {
-    markPageLoaded();
-  }, []);
+    setTimeout(markPageLoaded, 200);
+    fetchSavedProducts();
+  }, [])
+
   return (
     <>
+      <CartModal
+        productData={selectedProductData}
+        setProductData={setSelectedProductData}
+        productSnapshots={productSnapshots}
+        productFilteredVariantData={productFilteredVariantData}
+        selectedVariantData={selectedVariantData}
+        setSelectedVariantData={setSelectedVariantData}
+        handleImageChange={handleImageChange}
+        selectedVariantIndex={selectedVariantIndex}
+        setProductSnapshots={setProductSnapshots}
+        setProductFilteredVariantData={setProductFilteredVariantData}
+        bestSeller={bestSellers}
+        savedProductsData={savedProductsData}
+        setSavedProductsData={setSavedProductsData}
+      />
       <MarketIntroSection data={marketSection} />
-      <MarketBestSeller products={bestSellerProducts} />
+      <MarketBestSeller products={bestSellerProducts}  savedProductsData={savedProductsData} setSavedProductsData={setSavedProductsData} getSelectedProductSnapShots={getSelectedProductSnapShots} />
       <NewArrival content={newArrivalSectionContent} />
-      <Highlights pageContent={homeSectionDetails} data={highlightsSectionData} />
+      <Highlights pageContent={homeSectionDetails} data={highlightsSectionData} savedProductsData={savedProductsData} setSavedProductsData={setSavedProductsData} getSelectedProductSnapShots={getSelectedProductSnapShots} />
       <MarketSlider content={homeSectionDetails} marketSliderData={marketSliderData} />
       <PeopleReviewSlider data={peopleReviewSliderData} homeSectionDetails={homeSectionDetails} />
       <Markets marketsData={marketsData} />
