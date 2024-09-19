036cd3b0717b9e201b7b5a2b888bcd348e656a4f
Osyed002 <osyed@strugbitsglobal.com>
2024-09-19 15:27:00 +0500

    enable ssg for products

--- src/app/(root)/product/[slug]/page.jsx
+++ src/app/(root)/product/[slug]/page.jsx
@@ -53,7 +53,6 @@ export async function generateMetadata({ params }) {
 
 export const generateStaticParams = async () => {
   try {
-    return [];
     const paths = await fetchAllProductsPaths() || [];
     return paths;
   } catch (error) {
