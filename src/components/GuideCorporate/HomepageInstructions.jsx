de6312b7a5c8a0ea1917aa4a4e90a32551e17e54
Osyed002 <osyed@strugbitsglobal.com>
2025-01-08 21:17:50 +0500

    homepage instructions

--- src/components/GuideCorporate/HomepageInstructions.jsx
+++ src/components/GuideCorporate/HomepageInstructions.jsx
@@ -18,16 +18,94 @@ export const HomepageInstructions = ({ setTabIndex, DASHBOARD_URL }) => {
                 <li>
                     <p className="black-1">
                         <span className="blue-1">Get In Touch Section:</span> Use the <AnimateLink className="blue-1" target="_blank" to={`${CMS_URL}/GetInTouchSection`}>
-                        Home (Third Sec - Get In Touch)
+                            Home (Third Sec - Get In Touch)
                         </AnimateLink> collection to manage the get in touch section.
                     </p>
                 </li>
                 <li>
                     <p className="black-1">
                         <span className="blue-1">Portfolio/Projects section:</span> Use the <AnimateLink className="blue-1" target="_blank" to={`${CMS_URL}/PortfolioCollection`}>
-                        Portfolio
+                            Portfolio
                         </AnimateLink> collection to manage the portfolio section on the home page.
                     </p>
+                </li>
+                <li>
+                    <p className="black-1">
+                        <span className="blue-1">Reviews Slider/Here's what people are saying:</span> Use the <AnimateLink className="blue-1" target="_blank" to={`${CMS_URL}/PeopleReviewSlider`}>
+                            Reviews Slider
+                        </AnimateLink> collection to update the content of the reviews slider section.
+                    </p>
+                </li>
+                <li>
+                    <p className="black-1">
+                        <span className="blue-1">Rental Store Section:</span> Use the <AnimateLink className="blue-1" target="_blank" to={`${CMS_URL}/RentalStore`}>
+                        RentalStore
+                        </AnimateLink> collection to update the content of the Rental Store Section.
+                    </p>
+                    <ul>
+                        <li>
+                            <p className="black-1">
+                                <AnimateLink className="blue-1">
+                                    <span onClick={() => { setTabIndex(1) }}>Markets Section</span>
+                                </AnimateLink>
+                            </p>
+                        </li>
+                        <li>
+                            <p className="black-1">
+                                <AnimateLink className="blue-1">
+                                    <span onClick={() => { setTabIndex(1) }}>Studios Section</span>
+                                </AnimateLink>
+                            </p>
+                        </li>
+                        <li>
+                            <p className="black-1">
+                                <AnimateLink className="blue-1">
+                                    <span onClick={() => { setTabIndex(1) }}>Dream Big Section</span>
+                                </AnimateLink>
+                            </p>
+                        </li>
+                        <li>
+                            <p className="black-1">
+                                <AnimateLink className="blue-1">
+                                    <span onClick={() => { setTabIndex(1) }}>Let's Get Social Section</span>
+                                </AnimateLink>
+                            </p>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <p className='black-1'><span className="blue-1">Shared Sections:</span> Refer to the common collections/sections guide for instructions.
+                    </p>
+                    <ul>
+                        <li>
+                            <p className="black-1">
+                                <AnimateLink className="blue-1">
+                                    <span onClick={() => { setTabIndex(1) }}>Markets Section</span>
+                                </AnimateLink>
+                            </p>
+                        </li>
+                        <li>
+                            <p className="black-1">
+                                <AnimateLink className="blue-1">
+                                    <span onClick={() => { setTabIndex(1) }}>Studios Section</span>
+                                </AnimateLink>
+                            </p>
+                        </li>
+                        <li>
+                            <p className="black-1">
+                                <AnimateLink className="blue-1">
+                                    <span onClick={() => { setTabIndex(1) }}>Dream Big Section</span>
+                                </AnimateLink>
+                            </p>
+                        </li>
+                        <li>
+                            <p className="black-1">
+                                <AnimateLink className="blue-1">
+                                    <span onClick={() => { setTabIndex(1) }}>Let's Get Social Section</span>
+                                </AnimateLink>
+                            </p>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </>
