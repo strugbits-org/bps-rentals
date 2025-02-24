0e296d3b80eef61b10837f9f4f0f746ff4db997a
Osyed002 <osyed@strugbitsglobal.com>
2025-02-24 20:13:20 +0500

    improve search results

--- src/Services/FetchFunction.js
+++ src/Services/FetchFunction.js
@@ -70,9 +70,9 @@ const getDataFetchFunction = async (payload) => {
 
     if (search?.length === 2) {
       const words = search[1].split(/\s+/).filter(Boolean);
-      dataQuery = dataQuery.contains(search[0], words[0]);
+      dataQuery = dataQuery.contains(search[0], words[0] || "");
       for (let i = 1; i < words.length; i++) {
-        dataQuery = dataQuery.or(dataQuery.contains(search[0], words[i]));
+        dataQuery = dataQuery.and(dataQuery.contains(search[0], words[i]));
       }
     };
 
