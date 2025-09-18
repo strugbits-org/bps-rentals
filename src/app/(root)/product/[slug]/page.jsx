f5c9124bde6c4204e832c8010204c2867035b405
Osyed002 <osyed@strugbitsglobal.com>
2025-09-18 15:22:21 +0500

    Refactor SavedProducts component to remove unnecessary props and streamline data fetching; update static params for category and product pages to return all paths instead of a limited set. Clean up console logs in Navbar component.

--- src/app/(root)/product/[slug]/page.jsx
+++ src/app/(root)/product/[slug]/page.jsx
@@ -57,7 +57,7 @@ export async function generateMetadata({ params }) {
 export const generateStaticParams = async () => {
   try {
     const paths = await fetchAllProductsPaths() || [];
-    return paths.slice(0, 2);
+    return paths;
   } catch (error) {
     logError("Error generating static params(product page):", error);
     return [];
