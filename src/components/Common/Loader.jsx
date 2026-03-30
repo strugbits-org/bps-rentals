bc4446d6697e7ab8c5f0679c797ba6528116988a
osyed002 <osyed@strugbitsglobal.com>
2026-03-30 14:51:22 +0500

    Refactor: Remove unused SVG logo files and update Loader component to use SVG components

--- src/components/Common/Loader.jsx
+++ src/components/Common/Loader.jsx
@@ -1,3 +1,7 @@
+import LogoBpsB from "../svgs/LogoBpsB";
+import LogoBpsP from "../svgs/LogoBpsP";
+import LogoBpsS from "../svgs/LogoBpsS";
+
 const Loader = () => {
   return (
     <div id="loader">
@@ -6,13 +10,16 @@ const Loader = () => {
       </div>
       <div className="container-logo">
         <div className="z-5">
-          <img src="/images/logo/logo-bps-b.svg" alt="" className="img-b z-3" />
+          {/* <img src="/images/logo/logo-bps-b.svg" alt="" className="img-b z-3" /> */}
+          <LogoBpsB className="img-b z-3" />
         </div>
         <div className="z-4">
-          <img src="/images/logo/logo-bps-p.svg" alt="" className="img-p z-2" />
+          {/* <img src="/images/logo/logo-bps-p.svg" alt="" className="img-p z-2" /> */}
+          <LogoBpsP className="img-p z-3"/>
         </div>
         <div className="z-3">
-          <img src="/images/logo/logo-bps-s.svg" alt="" className="img-s z-1" />
+          {/* <img src="/images/logo/logo-bps-s.svg" alt="" className="img-s z-1" /> */}
+          <LogoBpsS className="img-s z-3"/>
         </div>
       </div>
     </div>
