65fc42907224636c0d18334c34450578ae903658
osyed002 <osyed@strugbitsglobal.com>
2026-06-08 20:10:30 +0500

    Enhance authentication cookie management by integrating user data handling

--- src/Utils/AuthSession.js
+++ src/Utils/AuthSession.js
@@ -1,3 +1,5 @@
+import { clearRequested, getUserKeyFromUserData } from "@/Utils/Request3dAccess";
+
 export const AUTH_REQUIRED = "AUTH_REQUIRED";
 
 export const isAuthErrorMessage = (message = "") => {
@@ -12,7 +14,10 @@ export const isAuthErrorMessage = (message = "") => {
 
 export const isAuthError = (error) => isAuthErrorMessage(error?.message);
 
-export const clearAuthCookies = (removeCookie) => {
+export const clearAuthCookies = (removeCookie, userData) => {
+  const userKey = getUserKeyFromUserData(userData);
+  if (userKey) clearRequested(userKey);
+
   removeCookie("authToken", { path: "/" });
   removeCookie("userData", { path: "/" });
   removeCookie("userTokens", { path: "/" });
